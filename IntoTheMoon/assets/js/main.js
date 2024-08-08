f120523747aad145d1e5b03a015835888642d5d7
AleckRdz <rdzaleck@gmail.com>
2024-08-07 19:06:24 -0600

    first demo

--- IntoTheMoon/assets/js/main.js
+++ IntoTheMoon/assets/js/main.js
@@ -14,11 +14,15 @@
    */
   const select = (el, all = false) => {
     el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
+    try {
+      if (all) {
+        return [...document.querySelectorAll(el)];
+      } else {
+        return document.querySelector(el);
+      }
+    } catch (error) {
+      
+    }    
   }
 
   /**
